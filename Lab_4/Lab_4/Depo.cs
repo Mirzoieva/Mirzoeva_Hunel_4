2fd4e447326d518c12b770046df9658bab5c693c
Mirzoieva <107805547+Mirzoieva@users.noreply.github.com>
2022-06-19 21:47:16 +0300

    Add files via upload

---
+++ Lab_4/Lab_4/Depo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TansportType;
+
+namespace Lab_4
+{
+    class Depo
+    {
+       
+        public List<Transport> transport = new List<Transport>();
+
+        public void Add( string name, double speed, double cost)
+        {
+            if(name=="train")
+            {
+                transport.Add(new Train(speed, cost));
+            }
+            else if (name =="airplane")
+            {
+                transport.Add(new Airplane(speed, cost));
+            }
+            else if (name=="car")
+            {
+                transport.Add(new Car(speed, cost));
+            }
+            else if (name =="bus")
+            {
+                transport.Add(new Bus (speed, cost));
+            }
+            else
+            {
+                transport.Add(new Helicopter(speed, cost));
+            }
+        }
+
+        public void Choose()
+        {
+            foreach (var value in transport)
+            {
+                Type t = value.GetType();
+                if (t.Equals(typeof(Train)))
+                {
+                    Console.WriteLine("Поїзд");
+                    value.Length();
+                    value.Price();
+                    value.Time();
+                }
+                else if (t.Equals(typeof(Bus)))
+                {
+                    Console.WriteLine("Автобус");
+                    value.Length();
+                    value.Price();
+                    value.Time();
+                }
+                else if (t.Equals(typeof(Car)))
+                {
+                    Console.WriteLine("Автомобiль");
+                    value.Length();
+                    value.Price();
+                    value.Time();
+                }
+                else if (t.Equals(typeof(Airplane)))
+                {
+                    Console.WriteLine("Лiтак");
+                    value.Length();
+                    value.Price();
+                    value.Time();
+                }
+                else if (t.Equals(typeof(Helicopter)))
+                {
+                    Console.WriteLine("Вертолiт");
+                    value.Length();
+                    value.Price();
+                    value.Time();
+                }
+            }
+        }
+    }
+}
---
+++ Lab_4/Lab_4/TransportType.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lab_4;
+
+
+namespace TansportType
+{
+    class Airplane : Transport
+    {
+     
+        public Airplane(double speed, double cost) : base(speed, cost) { }
+
+    }
+
+    class Train : Transport
+    {
+       
+        public Train (double speed, double cost) : base(speed, cost) { }
+
+    }
+    class Car : Transport
+    {
+       
+        public Car (double speed, double cost) : base(speed, cost) { }
+    }
+
+    class Bus : Transport 
+    {      
+        public Bus(double speed, double cost) : base(speed, cost) { }
+    }
+    class Helicopter : Transport 
+    {
+        public Helicopter(double speed, double cost) : base(speed, cost) { }
+    }
+
+
+}
